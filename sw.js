f2b0cde7226b045da88efcf012deaa87dd9c44a4
Jesus Rojas <jesus@invorious.com>
2024-04-11 23:52:38 -0500

    fix: update sw :bug:

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 const staticCacheName = `pwa-version-${new Date().getTime()}`;
 const filesToCache = [
-  "/assets/audio/cardinal.mp3",
-  "/assets/icons/favicon-16x16.png",
-  "/assets/icons/favicon-32x32.png",
-  "/assets/icons/favicon-96x96.png",
-  "/assets/icons/favicon-128x128.png",
-  "/assets/icons/favicon-196x196.png",
-  "/css/styles.css",
-  "/js/main.js",
-  "/index.html",
+  '/assets/audio/cardinal.mp3',
+  '/assets/icons/favicon-16x16.png',
+  '/assets/icons/favicon-32x32.png',
+  '/assets/icons/favicon-96x96.png',
+  '/assets/icons/favicon-128x128.png',
+  '/assets/icons/favicon-196x196.png',
+  '/css/styles.css',
+  '/js/main.js',
+  '/index.html',
 ];
 
 // Cache on install
@@ -17,7 +17,6 @@ self.addEventListener("install", (event) => {
     caches
       .open(staticCacheName)
       .then((cache) => cache.addAll(filesToCache))
-      .then(() => self.skipWaiting())
   );
 });
 
@@ -34,7 +33,6 @@ self.addEventListener("activate", (event) => {
             .map((cacheName) => caches.delete(cacheName))
         )
       ))
-      .then(() => self.clients.claim())
   );
 });
 
@@ -44,12 +42,5 @@ self.addEventListener("fetch", (event) => {
     caches
       .match(event.request)
       .then((response) => response || fetch(event.request))
-      .catch(() => caches.match('/index.html'))
   );
 });
-
-// Sync
-self.addEventListener('sync', console.log);
-
-// Push notifications
-self.addEventListener('push', console.log);
